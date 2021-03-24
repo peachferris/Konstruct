75634115d00e2c17192e13a900474fa6335c77e6
peachferris <52829920+peachferris@users.noreply.github.com>
2021-03-24 11:50:31 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+ })
+}
+
+burger.addEventListener('click', () => {
+    menu.classList.add('main-menu--visible');
+});
+
+close.addEventListener('click', () => {
+    main-menu.classList.remove('main-menu--visible');
+});
+
+
+
+
+
